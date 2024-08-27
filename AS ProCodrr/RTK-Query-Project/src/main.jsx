12c6741b765da2582dc8a1747f48e8f2dea3abde
shubham2o <2contactshubham@gmail.com>
2024-08-27 18:06:29 +0530

    Basic installation and setup completed

---
+++ AS ProCodrr/RTK-Query-Project/src/main.jsx
@@ -0,0 +1,26 @@
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <p>Contact Us</p>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
