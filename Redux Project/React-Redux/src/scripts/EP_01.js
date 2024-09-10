8a9f85b532c0d8164b11c75abd87f95155345612
shubham2o <2contactshubham@gmail.com>
2024-09-10 16:40:30 +0530

    EP_01 completed

--- Redux Project/React-Redux/src/scripts/EP_01.js
+++ Redux Project/React-Redux/src/scripts/EP_01.js
@@ -1,3 +1,9 @@
+import { createStore } from "redux";
+
+/* -----------------------
+--------- CASE 1 ---------
+------------------------ */
+
 // const person = {
 //     name: "Shubham",
 //     age: 24,
@@ -60,38 +66,92 @@
 // console.log('copyPerson', copyPerson.hobbies[0]);
 // console.log('person', person.hobbies[0]);
 
-let state = {
-    count: 0,
+/* -----------------------
+--------- CASE 2 ---------
+------------------------ */
+
+// let state = {
+//     count: 0,
+// }
+
+// let prevState = state;
+
+// console.log("state", state.count);
+// console.log("prevState", prevState.count);
+
+// const increment = () => {
+//     // state.count += 1;
+//     /* Mutating State -
+//     It will change the original state object as well, which is not the ideal case.
+//     This happens because it creates a reference (pointing) to the original state object
+//     in the memory, which means both the objects are pointing to the same state object or
+//     has the same state object's value. */
+
+//     state = { ...state, count: state.count + 1 };
+//     /* Not Mutating State -
+//     It will not change the original state object, as it will create a new copy of the
+//     state object in the memory. It will store a copy of the original state object's value
+//     in the new object. */
+// }
+
+// console.log("state", state.count);
+// console.log("prevState", prevState.count);
+// increment();
+// console.log("state", state.count);
+// console.log("prevState", prevState.count);
+// increment();
+// console.log("state", state.count);
+// console.log("prevState", prevState.count);
+// increment();
+// console.log("state", state.count);
+// console.log("prevState", prevState.count);
+
+/* -----------------------
+--------- CASE 3 ---------
+------------------------ */
+
+const initialState = {
+    post: 0,
+    name: "Shubham Sharma",
+    age: 24,
 }
 
-let prevState = state;
+const INCREMENT = "post/increment";
+const DECREMENT = "post/decrement";
 
-console.log("state", state.count);
-console.log("prevState", prevState.count);
-
-const increment = () => {
-    // state.count += 1;
-    //*** Mutating State ***// 
-    // It will change the original state object as well, which is not the ideal case. 
-    // This happens because it creates a reference (pointing) to the original state object 
-    // in the memory, which means both the objects are pointing to the same state object or 
-    // has the same state object's value.
-
-    state = { count: state.count + 1 }
-    //*** Not Mutating State ***//
-    // It will not change the original state object, as it will create a new copy of the 
-    // state object in the memory. It will store a copy of the original state object's value 
-    // in the new object.
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return { ...state, post: state.post + action.payload }
+        case DECREMENT:
+            return { ...state, post: state.post - action.payload }
+        default:
+            return state;
+    }
 }
 
-console.log("state", state.count);
-console.log("prevState", prevState.count);
-increment();
-console.log("state", state.count);
-console.log("prevState", prevState.count);
-increment();
-console.log("state", state.count);
-console.log("prevState", prevState.count);
-increment();
-console.log("state", state.count);
-console.log("prevState", prevState.count);
+// -------------------- BUILT SMALL CUSTOM REDUX --------------------
+// console.log(reduxState);
+// reduxState = reducer(reduxState, { type: INCREMENT, payload: 100 });
+// console.log(reduxState);
+// reduxState = reducer(reduxState, { type: "DEFAULT_STATE" });
+// console.log(reduxState);
+// reduxState = reducer(reduxState, { type: DECREMENT, payload: 10 });
+// console.log(reduxState);
+
+// -------------------- HOW TO CREATE A STORE IN REDUX? --------------------
+const store = createStore(reducer);
+console.log(store);
+
+// -------------------- HOW TO ACCESS A STATE IN REDUX? --------------------
+console.log(store.getState());
+
+// -------------------- HOW TO TRACK STATE CHANGES (DISPATCH ACTIONS) IN REDUX? --------------------
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// -------------------- HOW TO CALL A REDUCER IN REDUX? --------------------
+store.dispatch({ type: INCREMENT, payload: 100 });
+store.dispatch({ type: "EXECUTE_DEFAULT_VALUE_IGNORE_PAYLOAD", payload: 1000 });
+store.dispatch({ type: DECREMENT, payload: 10 });
