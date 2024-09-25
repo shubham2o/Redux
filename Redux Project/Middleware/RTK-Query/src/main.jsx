487ea630426d9f7f30f45747c7f3f18be2dc09d9
shubham2o <2contactshubham@gmail.com>
2024-09-25 19:31:43 +0530

    EP_21 completed - RTK Query, CRUD Operations, Caching & Optimistic Updates

--- Redux Project/Middleware/RTK-Query/src/main.jsx
+++ Redux Project/Middleware/RTK-Query/src/main.jsx
@@ -1,5 +1,7 @@
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { api } from "./apiSlice.js";
 import App from "./App.jsx";
 import Home from "./Home.jsx";
 import ReactDOM from "react-dom/client";
@@ -7,11 +9,11 @@ import ReactDOM from "react-dom/client";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <ApiProvider api={api}><App /></ApiProvider>,
     children: [
       { path: "/", element: <Home /> },
       { path: "/contact", element: <p>Contact Us</p> }
-    ],
+    ]
   }
 ]);
 
